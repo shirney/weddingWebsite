7b69dc889f423df0fde99d49d5c94f7077cfd62c
achiao <kuochiaohuang@gmail.com>
2015-08-04 01:04:36 +0800

    Add debug button for changing theme

--- ui/js/main.js
+++ ui/js/main.js
@@ -3,9 +3,27 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 50
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+    });
+    $(aboutUsContainer).height($(aboutUsImg).height());
+    var removeBodyClass = function() {
+    	$('body').removeClass('NTU');
+    	$('body').removeClass('Garden');
+    	$('body').removeClass('Beach');
+    }
+    $(NTU).on('click', function(){
+    	removeBodyClass();
+    	$('body').addClass('NTU')
+    });
+    $(Garden).on('click', function(){
+    	removeBodyClass();
+    	$('body').addClass('Garden')
+    });
+    $(Beach).on('click', function(){
+    	removeBodyClass();
+    	$('body').addClass('Beach')
     });
 });
 var fadeTime = 800;
@@ -15,4 +33,3 @@ $(aboutUsImg).on("mouseenter", function(){
 $(aboutUsImg).on("mouseout", function(){
 	$('#aboutUsImg').stop().animate({opacity:1}, fadeTime);
 });
-$(aboutUsContainer).height($(aboutUsImg).height());

98dfa6967f2ff747d9f29b3dc3a2d88744f8ed3d
shirney <shirneyhuang@gmail.com>
2015-08-03 22:27:32 +0800

    Change home images and code refactor

--- ui/js/main.js
+++ ui/js/main.js
@@ -10,16 +10,9 @@ $(function() {
 });
 var fadeTime = 800;
 $(aboutUsImg).on("mouseenter", function(){
-	//$('#aboutUsImgHover').stop().animate({opacity:1}, fadeTime);
     $('#aboutUsImg').stop().animate({opacity:0}, fadeTime);
 });
 $(aboutUsImg).on("mouseout", function(){
 	$('#aboutUsImg').stop().animate({opacity:1}, fadeTime);
-    //$('#aboutUsImgHover').stop().animate({opacity:0}, fadeTime);
-});
-$(aboutUsImgHover).on("mouseout", function(){
-	console.log("out");
-	$('#aboutUsImg').animate({opacity:1}, fadeTime);
-    $('#aboutUsImgHover').animate({opacity:0}, fadeTime);
 });
 $(aboutUsContainer).height($(aboutUsImg).height());
